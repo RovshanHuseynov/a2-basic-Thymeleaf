7dd5e5ea9569ffac0f7906c28a9b09a05a5de58b
RovshanHuseynov <rovsen.huseyn.oglu@gmail.com>
2020-07-21 15:04:41 +0400

    -- data from form2 is successfully transfered to from3

--- src/main/java/com/rh/sprin/a2basicthymeleaf/controller/TestController.java
+++ src/main/java/com/rh/sprin/a2basicthymeleaf/controller/TestController.java
@@ -3,22 +3,23 @@ package com.rh.sprin.a2basicthymeleaf.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class TestController {
+    @RequestMapping("/begin")
+    public String begin(){
+        return "form1";
+    }
+
     @RequestMapping("/showForm")
     public String show(){
         return "form2";
     }
 
     @RequestMapping("/processForm")
-    public String process(Model model) {
-        model.addAttribute("studentName", "aaaaa");
+    public String process(@RequestParam("studentName") String s, Model model){
+        model.addAttribute("studentName", s);
         return "form3";
-    }
-
-    @RequestMapping("/begin")
-    public String begin(){
-        return "form1";
     }
 }
